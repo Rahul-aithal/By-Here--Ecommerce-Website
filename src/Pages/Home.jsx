66ae5440d1c8867485b74315f6f4f09fc8f3bcda
Rahul-aithal <aithalrahul34@gmail.com>
2024-07-20 15:01:04 +0530

    Changed Path all-items-page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,5 @@
 import ItemCard from '../Components/ItemCard';
 import React from 'react'
-import useFetch from '../Hooks/useFetchData'
 import { useDispatch, useSelector } from 'react-redux';
 import { setCategoryToogle } from '../Store/categorySlice'
 import { Button } from '@chakra-ui/react';
@@ -8,18 +7,17 @@ import { useNavigate } from 'react-router-dom';
 
 function Home() {
 
-  // const { data, loading, error } = useFetch('https://dummyjson.com/products/categories');
-  // console.log(data);
   const categorey = useSelector((state) => (state.category.category));
   console.log(categorey);
   const dispatch = useDispatch();
-  const navigate=useNavigate()
+  const navigate=useNavigate();
+  
   return (
     <div className='mx-2 h-full md:grid-cols-1 gap-3 relative' >
       <div className='relative my-2'>
 
         <h1 className='font-bold text-xl'>Bestseller</h1>
-        <h2 className='absolute right-0 top-0 mx-2 cursor-pointer text-black hover:text-blue-500' onClick={()=>{navigate("/ALLItemPage", { state: { bestSellers:true} })}}>See more...</h2>
+        <h2 className='absolute right-0 top-0 mx-2 cursor-pointer text-black hover:text-blue-500' onClick={()=>{navigate("/all-items-page", { state: { bestSellers:true} })}}>See more...</h2>
         <div className='grid md:grid-cols-4 grid-cols-1  gap-2 place-items-center'>
 
           <ItemCard url={'https://dummyjson.com/products?limit=4&sortBy=rating&order=desc'} />
@@ -33,7 +31,10 @@ function Home() {
           }}>
             {categorey.name}
           </Button> </h1>
-          <h2 className='absolute right-0 top-0 mx-2 cursor-pointer text-black hover:text-blue-500' >See more...</h2>
+          <h2 className='absolute right-0 top-0 mx-2 cursor-pointer text-black hover:text-blue-500'
+           onClick={()=>{navigate("/all-items-page", { state: { bestSellers:false} })}} >
+            See more...
+            </h2>
         <div className='grid md:grid-cols-4 grid-cols-1 gap-2 place-items-center'>
           <ItemCard url={`https://dummyjson.com/products/category/${categorey.slug}?limit=4&sortBy=rating&order=desc`} />
         </div>
