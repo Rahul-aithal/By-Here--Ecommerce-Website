fb0567a2802b352046b56a60b405d15be45d15e1
Rahul-aithal <aithalrahul34@gmail.com>
2024-07-27 13:05:08 +0530

    all itmes added

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -3,6 +3,6 @@ export { default as About } from './About';
 export { default as Support } from './Support';
 export { default as Cart } from './Cart';
 export {default as Blog} from './Blog';
-export {default as AllItmes} from './AllItmes'
+export {default as AllItmes} from './All-Itmes'
 export {default as BlogDetail} from './BlogPage'
 export {default as ItemPage} from '../Components/ItemPage'
