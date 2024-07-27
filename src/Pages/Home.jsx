0b53c8c38324a36c5a64fa99fe0b69cbafdc9215
Rahul-aithal <aithalrahul34@gmail.com>
2024-07-27 13:00:03 +0530

    dark mode added

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -14,9 +14,9 @@ function Home() {
   return (
     <div className='mx-2 h-full md:grid-cols-1 gap-3 relative'>
       <div className='relative my-2'>
-        <h1 className='font-bold text-xl'>Bestseller</h1>
+        <h1 className='font-bold text-xl dark:text-white'>Bestseller</h1>
         <h2
-          className='absolute right-0 top-0 mx-2 cursor-pointer text-black hover:text-blue-500'
+          className='absolute right-0 top-0 mx-2 cursor-pointer text-black dark:text-white hover:text-blue-500'
           onClick={() => navigate("/all-items-page", { state: { bestSellers: true } })}
         >
           See more...
@@ -27,17 +27,17 @@ function Home() {
       </div>
 
       <div className='relative my-3'>
-        <h1 className='font-semibold'>
+        <h1 className='font-bold  dark:text-white '>
           Category:
           <button
-            className='font-bold mx-2 hover:shadow-xl bg-gray-200 p-2 rounded'
+            className='font-bold mx-2 hover:shadow-xl bg-gray-200 dark:bg-gray-800 p-2 rounded'
             onClick={() => dispatch(setCategoryToogle({ categoryToogle: true }))}
           >
             {category.name}
           </button>
         </h1>
         <h2
-          className='absolute right-0 top-0 mx-2 cursor-pointer text-black hover:text-blue-500'
+          className='absolute right-0 top-0 mx-2 cursor-pointer text-black dark:text-white hover:text-blue-500'
           onClick={() => navigate("/all-items-page", { state: { bestSellers: false } })}
         >
           See more...
@@ -48,54 +48,70 @@ function Home() {
       </div>
 
       {/* Our Blogs starts */}
-      <Box m="6">
-      <Heading as="h1" size="lg" fontWeight="bold" mb="4">
-        Our Blog
-      </Heading>
-      <Grid 
-        templateColumns={{ base: 'repeat(1, 1fr)', md: 'repeat(2, 1fr)', lg: 'repeat(3, 1fr)' }} 
-        gap={6}
-      >
-        {blogPosts.map((post) => (
-          <Box
-            key={post.id}
-            maxW="sm"
-            borderWidth="1px"
-            borderRadius="lg"
-            overflow="hidden"
-            boxShadow="md"
-            transition="transform 0.3s, box-shadow 0.3s"
-            _hover={{ transform: 'scale(1.02)', boxShadow: 'lg' }}
-          >
-            <Image
-              src={post.image}
-              alt={post.title}
-              h="56"
-              w="full"
-              objectFit="fit"
-            />
-            <VStack align="start" p="4" spacing="3">
-              <Heading as="h2" size="md" isTruncated>
-                {post.title}
-              </Heading>
-              <Text color="gray.500" noOfLines={3}>
-                {post.content}
-              </Text>
-              <Box display="flex" justifyContent="space-between" alignItems="center" w="full">
-                <Button
-                  colorScheme="blue"
-                  variant="link"
-                  onClick={() => navigate(`/blogs/${post.id}`)}
+      <Box m="6" position="relative">
+        <Heading as="h1" size="lg" fontWeight="bold" mb="4" className='dark:text-white'>
+          Our Blog
+        </Heading>
+        <Grid
+          templateColumns={{ base: 'repeat(1, 1fr)', md: 'repeat(2, 1fr)', lg: 'repeat(3, 1fr)' }}
+          gap={6}
+          placeItems={"Center"}
+        >
+          {blogPosts.map((post, index) => (
+            index < 3 &&
+            <Box
+              key={post.id}
+              maxW="sm"
+              borderWidth="1px"
+              borderRadius="lg"
+              overflow="hidden"
+              boxShadow="md"
+              transition="transform 0.3s, box-shadow 0.3s"
+              _hover={{ transform: 'scale(1.02)', boxShadow: 'lg' }}
+            >
+              <Image
+                src={post.image}
+                alt={post.title}
+                h="56"
+                w="full"
+                objectFit="cover"
+              />
+              <VStack align="start" p="4" spacing="3">
+                <Heading
+                  size="md"
+                  noOfLines={2}
+                  className="dark:text-white text-sm"
                 >
-                  Read More
-                </Button>
-              </Box>
-            </VStack>
-          </Box>
-        ))}
-      </Grid>
-    </Box>
-    
+                  {post.title}
+                </Heading>
+                <Text
+                  color="gray.500"
+                  noOfLines={3}
+                >
+                  {post.content}
+                </Text>
+                <Box display="flex" justifyContent="space-between" alignItems="center" w="full">
+                  <Button
+                    colorScheme="blue"
+                    variant="link"
+                    onClick={() => navigate(`/blogs/${post.id}`)}
+                  >
+                    Read More
+                  </Button>
+                </Box>
+              </VStack>
+            </Box>
+
+          ))}
+
+        </Grid>
+        <Text position='absolute' top="0" right="2" onClick={() => {
+          navigate("/blogs")
+        }} className='dark:text-white hover:text-blue-500 cursor-pointer' >
+          See more blogs...
+        </Text>
+      </Box>
+
     </div >
   );
 }
