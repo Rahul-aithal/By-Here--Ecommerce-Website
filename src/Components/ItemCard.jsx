5bdc1c60b6f523bb27a70b95e8855cc8d79d1b7e
Rahul-aithal <aithalrahul34@gmail.com>
2024-07-27 16:32:37 +0530

    small bugs

--- src/Components/ItemCard.jsx
+++ src/Components/ItemCard.jsx
@@ -22,7 +22,7 @@ const handleClick=function (data) {
             <div key={data.id} onClick={()=>{handleClick(data)}}>
                 <div  className='flex flex-col p-2 w-56 cursor-pointer'>
                     <button className='relative center rounded-3xl shadow-lg shadow-gray-500 bg-gray-50 dark:bg-gray-800 h-56 text-center'>
-                        <span className='p-1 absolute left-6 top-2 transition-all ease-linear bg-white rounded-full shadow-xl w-14 z-10'>{data.rating}</span>
+                        <span className='p-1 absolute left-6 top-2 transition-all ease-linear bg-white dark:bg-zinc-950 dark:text-white text-black rounded-full shadow-xl w-14 z-10'>{data.rating}</span>
                         {data.thumbnail ?
                             <img className='w-48 absolute top-5 right-2 transition-all ease-in fill-none'
                                 src={data.thumbnail}
